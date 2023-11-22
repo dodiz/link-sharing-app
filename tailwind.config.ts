0027ea6486d31702ee83d02de68b76f796ef702a
Donato Di Zenzo <donato.dizenzo96@gmail.com>
2023-11-22 11:45:08 +0100

    feat: add global styling

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,57 @@
 import type { Config } from "tailwindcss";
-
+const baseFontSize = 10;
 const config: Config = {
   content: [
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./ui/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          100: "#EFEBFF",
+          200: "#B3ADFF",
+          300: "#633CFF",
+        },
+        secondary: {
+          100: "#ffffff",
+          200: "#FAFAFA",
+          300: "#D9D9D9",
+          400: "#737373",
+          500: "#333333",
+        },
+        error: "#FF3939",
+      },
+      screens: {
+        md: "768px",
+        lg: "1024px",
+      },
+      fontSize: {
+        xl: "3.2rem",
+        base: "1.6rem",
+        xs: "1.2rem",
+      },
+      spacing: () => ({
+        ...Array.from({ length: 96 }, (_, index) => index * 0.5)
+          .filter((i) => i)
+          .reduce(
+            (acc, i) => ({ ...acc, [i]: `${i / (baseFontSize / 4)}rem` }),
+            {}
+          ),
+      }),
+      borderWidth: {
+        1: ".1rem",
+      },
+      borderRadius: {
+        sm: "0.8rem",
+        base: "1.2rem",
+      },
+      boxShadow: {
+        base: "0 0 32px 0 rgba(0, 0, 0, 0.10)",
+        accent: "0 0 32px 0 rgba(99, 60, 255, 0.25)",
+      },
+    },
   },
   plugins: [],
 };
