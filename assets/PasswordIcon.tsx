e6b5429f797419e31d7e4e8baf5693cdfce615f4
Donato Di Zenzo <donato.dizenzo96@gmail.com>
2023-12-01 20:44:37 +0100

    feat: add few icons

---
+++ assets/PasswordIcon.tsx
@@ -0,0 +1,16 @@
+import { ComponentProps, FC } from "react";
+
+export const PasswordIcon: FC<ComponentProps<"svg">> = (props) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="16"
+      height="16"
+      viewBox="0 0 16 16"
+      fill="#737373"
+      {...props}
+    >
+      <path d="M13 5h-2V3.5a3 3 0 0 0-6 0V5H3a1 1 0 0 0-1 1v7a1 1 0 0 0 1 1h10a1 1 0 0 0 1-1V6a1 1 0 0 0-1-1ZM8.5 9.914V11.5a.5.5 0 0 1-1 0V9.914a1.5 1.5 0 1 1 1 0ZM10 5H6V3.5a2 2 0 1 1 4 0V5Z" />
+    </svg>
+  );
+};
