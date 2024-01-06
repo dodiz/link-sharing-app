73850f1309f8f1e2d9d42a96742d3e5fde50f112
Donato Di Zenzo <donato.dizenzo96@gmail.com>
2024-01-06 15:24:24 +0100

    feat: init backend routes

--- server/api/root.ts
+++ server/api/root.ts
@@ -1,11 +1,42 @@
+import { z } from "zod";
 import { inferRouterInputs, inferRouterOutputs } from "@trpc/server";
 import { createTRPCRouter, publicProcedure } from "./init";
 
+let socials: {
+  id: string;
+  url: string;
+}[] = [
+  {
+    id: "twitter",
+    url: "https://twitter.com/dodiz",
+  },
+];
+
 export const appRouter = createTRPCRouter({
-  greetings: createTRPCRouter({
-    hello: publicProcedure.query(() => {
-      return "Hello";
+  socials: createTRPCRouter({
+    getAll: publicProcedure.query(() => {
+      return socials;
     }),
+    update: publicProcedure
+      .input(
+        z.array(
+          z.object({
+            url: z.string().url(),
+            id: z.string(),
+            action: z.enum(["add", "remove"]),
+          })
+        )
+      )
+      .mutation(({ input }) => {
+        const newSocials = input.filter((social) => social.action === "add");
+        const removedSocials = input.filter(
+          (social) => social.action === "remove"
+        );
+        socials.push(...newSocials);
+        socials = socials.filter(
+          (social) => !removedSocials.find((s) => s.id === social.id)
+        );
+      }),
   }),
 });
 
