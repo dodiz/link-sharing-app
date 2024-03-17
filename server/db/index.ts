ca8902b8c38711c56206548abf48bbe30286c66c
Donato Di Zenzo <donato.dizenzo96@gmail.com>
2024-03-17 23:42:05 +0100

    feat: update db connection

--- server/db/index.ts
+++ server/db/index.ts
@@ -1,12 +1,14 @@
 import { drizzle } from "drizzle-orm/planetscale-serverless";
-import { connect } from "@planetscale/database";
+import { Client } from "@planetscale/database";
 import * as schema from "./schema";
 import { env } from "@/env";
 
-const connection = connect({
-  url: env.DATABASE_URI,
+const client = new Client({
+  host: env.DATABASE_HOST,
+  username: env.DATABASE_USERNAME,
+  password: env.DATABASE_PASSWORD,
 });
 
-export const db = drizzle(connection, {
+export const db = drizzle(client, {
   schema,
 });
