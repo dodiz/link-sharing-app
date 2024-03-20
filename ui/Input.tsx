378469391fc538fc9a5fbaf5a944cf75a9af5bb1
Donato Di Zenzo <donato.dizenzo96@gmail.com>
2024-03-20 09:39:52 +0100

    feat: input is now a rsc

--- ui/Input.tsx
+++ ui/Input.tsx
@@ -1,51 +1,29 @@
-"use client";
-
-import { ComponentProps, FC, useState } from "react";
+import { ComponentProps, FC, ReactNode } from "react";
 import { Typography } from "@/ui/Typography";
 import { cn } from "@/utils/cn";
 
 type InputProps = ComponentProps<"input"> & {
   label?: string;
   error?: string;
-  Icon?: FC<ComponentProps<"svg">>;
+  Icon?: ReactNode;
 };
 
 export const Input: FC<InputProps> = ({
   label,
-  onFocus,
-  onBlur,
   error,
   Icon,
   className,
   ...rest
 }) => {
-  const [isFocused, setIsFocused] = useState(false);
   return (
     <label className={cn("flex flex-col gap-1", className)}>
       {label && <Typography variant="body-s">{label}</Typography>}
       <div
-        className={`flex items-center gap-2 rounded-sm border-1 bg-secondary-50 px-4 py-3 text-secondary-500 transition-all ${
-          isFocused
-            ? "border-primary-300 shadow-accent"
-            : error
-              ? "border-error text-error"
-              : "border-secondary-300"
-        }`}
+        data-state={error ? "error" : "default"}
+        className={`flex items-center gap-2 rounded-sm border-1 border-secondary-300 bg-secondary-50 px-4 py-3 text-secondary-500 transition-all focus-within:border-primary-300 focus-within:shadow-accent data-[state=error]:border-error [&:not(:focus-within)]:data-[state=error]:text-error `}
       >
-        {Icon && <Icon />}
-        <input
-          onFocus={(e) => {
-            setIsFocused(true);
-            onFocus?.(e);
-          }}
-          onBlur={(e) => {
-            setIsFocused(false);
-            onBlur?.(e);
-          }}
-          className="flex-1 outline-none"
-          type="text"
-          {...rest}
-        />
+        {Icon}
+        <input className="flex-1 outline-none" type="text" {...rest} />
         {error && (
           <div className="whitespace-nowrap text-xs text-error">{error}</div>
         )}
