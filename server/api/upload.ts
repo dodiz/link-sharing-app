62c830dd3522bb6aefaa82d922ed5390044563f6
Donato Di Zenzo <donato.dizenzo96@gmail.com>
2024-04-04 21:05:44 +0200

    feat: 1 image per user

--- server/api/upload.ts
+++ server/api/upload.ts
@@ -4,14 +4,32 @@ import {
 } from "uploadthing/next";
 import { UploadThingError } from "uploadthing/server";
 import { getServerAuthSession } from "@/server/auth";
+import { UTApi } from "uploadthing/server";
+import { env } from "@/env";
+import { db } from "@/server/db";
+
+const utApi = new UTApi({ apiKey: env.UPLOADTHING_SECRET });
 
 const f = createUploadthing();
 
 export const fileRouter = {
-  imageUploader: f({ image: { maxFileSize: "64KB" } })
-    .middleware(async ({ req }) => {
+  imageUploader: f({ image: { maxFileSize: "64KB", maxFileCount: 1 } })
+    .middleware(async () => {
       const session = await getServerAuthSession();
       if (session === null) throw new UploadThingError("Unauthorized");
+      const user = await db.query.profile.findFirst({
+        where: (profile, { eq }) => eq(profile.user, session.user.email!),
+      });
+      if (!user) throw new UploadThingError("User not found");
+      /**
+       * Delete the old image if it exists
+       */
+      if (user.image) {
+        const imageId = user.image.split("/").pop();
+        if (imageId) {
+          await utApi.deleteFiles(imageId);
+        }
+      }
       return { user: session.user };
     })
     .onUploadComplete(async ({ metadata, file }) => {
